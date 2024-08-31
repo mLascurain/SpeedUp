9317e2395e8250bdd3c668d425a9d775944c6bd5
Matias Lascurain <matiaslascurain04@gmail.com>
2024-08-31 09:00:15 -0300

    added some comments

--- main.js
+++ main.js
@@ -6,10 +6,13 @@ document.getElementById('speed').addEventListener('click', () => {
   getSpeed()
 });
 
+// Retrieves a numeric value from an input element with the ID speed and calls setVideoSpeed()
 function getSpeed(){
   const speed = parseFloat(document.getElementById('speed').value);
 
+  // Query the currently active tab in the current window.
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    // Execute a script in the context of the active tab.
     chrome.scripting.executeScript({
       target: { tabId: tabs[0].id, allFrames: true },
       func: setVideoSpeed,
@@ -18,6 +21,7 @@ function getSpeed(){
   });
 }
 
+// Adjust the playback speed of all video elements on a webpage
 function setVideoSpeed(speed) {
   const videos = document.querySelectorAll('video');
   videos.forEach(video => {
